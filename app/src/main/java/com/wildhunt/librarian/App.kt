d5adc2984762ddca1fc88f3ae71ca6f630bda6a0
NickHuk <mykola.huk@uptech.team>
2022-01-22 12:33:25 +0200

    initial

--- app/src/main/java/com/wildhunt/librarian/App.kt
+++ app/src/main/java/com/wildhunt/librarian/App.kt
@@ -4,6 +4,6 @@ import android.app.Application
 import com.wildhunt.librarian.di.AppComponent
 import com.wildhunt.librarian.di.AppComponentProvider
 
-class App : Application(), AppComponentProvider {
+class  App : Application(), AppComponentProvider {
   override val component: AppComponent by lazy { AppComponent.get(applicationContext) }
 }
