a2a901ca628a1165c32a30d1b18deb3d25f04bab
rodionkushch <rodion.kushch@uptech.team>
2022-01-22 18:01:12 +0200

    add audio recording

---
+++ app/src/main/java/com/wildhunt/librarian/AudioPlayer.kt
@@ -0,0 +1,22 @@
+package com.wildhunt.librarian
+
+import android.media.MediaPlayer
+import android.util.Log
+import java.io.IOException
+
+class AudioPlayer {
+
+  private var player: MediaPlayer? = null
+
+  fun startPlaying(fileName: String) {
+    player = MediaPlayer().apply {
+      try {
+        setDataSource(fileName)
+        prepare()
+        start()
+      } catch (e: IOException) {
+        Log.e(AudioPlayer::class.java.simpleName, "prepare() failed")
+      }
+    }
+  }
+}
